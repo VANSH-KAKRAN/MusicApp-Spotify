3875bb79edbf5cf4bbb4d98b543acdd801951e86
vansh kakran <vanshkakran82@gmail.com>
2024-06-29 18:11:59 +0530

    src

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -13,7 +13,7 @@ function Menu({ toggle }) {
               <div className="container-fluid">
                 <a className="navbar-brand fw-bold" href="#">
                   <img
-                    src="src/images/spotifylogo.png"
+                    src="../images/spotifylogo.png"
                     alt="Logo"
                     width="40"
                     height="40"
