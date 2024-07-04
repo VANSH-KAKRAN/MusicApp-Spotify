e5c4897337699b900169e79c41d882a6de2c8c80
vansh kakran <vanshkakran82@gmail.com>
2024-07-04 21:52:33 +0530

    pushing last of today

--- src/components/Cards/AudioPlayer.jsx
+++ src/components/Cards/AudioPlayer.jsx
@@ -73,7 +73,7 @@ const AudioPlayer = ({ songs }) => {
   };
 
   return (
-    <div>
+    <div className="Background-all">
       <div className="Play-follow3d">
         <li>
           <svg
@@ -137,6 +137,28 @@ const AudioPlayer = ({ songs }) => {
       </div>
       <div className="Popular-text">Popular</div>
       <ul className="playlist">
+        <div>
+          <ul className="TitleNav">
+
+          <li>#</li>
+          <li>Title</li>
+          <li className="popuarity">Popularity</li>
+          <li>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-clock"
+              viewBox="0 0 16 16"
+              >
+              <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z" />
+              <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0" />
+            </svg>
+          </li>
+              </ul>
+              <hr id="hr" />
+        </div>
         {songs.map((song, index) => (
           //   <li key={index} onClick={() => setCurrentTrack(index)}>
           //     {song.title} {song.popularity}{song.time}
@@ -150,8 +172,12 @@ const AudioPlayer = ({ songs }) => {
             <ul>
               <li className="Title">{song.title}</li>
               <li className="ArtistName">
-                <li>{song.Artist1} ,<br /> </li>
-                <li>{song.Artist2} ,<br /> </li>
+                <li>
+                  {song.Artist1} ,<br />{" "}
+                </li>
+                <li>
+                  {song.Artist2} ,<br />{" "}
+                </li>
                 <li>{song.Artist3}</li>
               </li>
             </ul>
