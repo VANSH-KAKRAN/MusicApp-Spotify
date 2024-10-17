71c4d8928810bfe6dc4c2a456af7a34498f0c2ec
VANSH-KAKRAN <vanshkakran82@gmail.com>
2024-10-17 19:20:48 +0530

    almost finished

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -8,8 +8,8 @@ function Search({ toggleSearch }) {
           <span className="input-group-text">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
+              width="20"
+              height="20"
               fill="currentColor"
               className="bi bi-search"
               viewBox="0 0 16 16"
